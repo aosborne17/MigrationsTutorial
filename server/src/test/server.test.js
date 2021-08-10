e04d81a28bbc694f09d4a4298055da1ab435f60c
Andrew Osborne <andrew@Andrews-MacBook-Pro.local>
2021-08-10 10:41:49 +0100

    e

--- server/src/test/server.test.js
+++ server/src/test/server.test.js
@@ -1,18 +1,21 @@
-const request = require("supertest");
-const { server } = require("../app");
+// const request = require("supertest");
+// const { server } = require("../app");
 
-it("Error when trying to signin with non existent email", async () => {
-  await request(server)
-    .get("/api/user")
-    .expect(200)
-    .then((response) => {
-      // Check the response type and length
-      expect(Array.isArray(response.body)).toBeTruthy();
-      //       expect(response.body.length).toEqual(1);
+// it("Error when trying to signin with non existent email", async () => {
+//   await request(server)
+//     .get("/api/user")
+//     .expect(200)
+//     .then((response) => {
+//       // Check the response type and length
+//       expect(Array.isArray(response.body)).toBeTruthy();
+//       //       expect(response.body.length).toEqual(1);
 
-      // Check the response data
-      //       expect(response.body[0]._id).toBe(post.id);
-      //       expect(response.body[0].title).toBe(post.title);
-      //       expect(response.body[0].content).toBe(post.content);
-    });
-});
+//       // Check the response data
+//       //       expect(response.body[0]._id).toBe(post.id);
+//       //       expect(response.body[0].title).toBe(post.title);
+//       //       expect(response.body[0].content).toBe(post.content);
+//     })
+//     .then(() => {
+//       console.log("done");
+//     });
+// });
